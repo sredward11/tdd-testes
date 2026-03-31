bfac315678e52f00c1f985665f09380facd1556d
Carl-edd <caduzecavalim@gmail.com>
2026-03-31 10:47:17 -0300

    green: implementa filterTasks

--- src/taskManager.js
+++ src/taskManager.js
@@ -55,3 +55,18 @@ export function toggleTask(task) {
 export function removeTask(tasks, taskId) {
   return tasks.filter((task) => task.id !== taskId);
 }
+// ------------------------------------------------------------
+// Filtros
+// ------------------------------------------------------------
+
+export function filterTasks(tasks, status) {
+  switch (status) {
+    case 'completed':
+      return tasks.filter((task) => task.completed === true);
+    case 'pending':
+      return tasks.filter((task) => task.completed === false);
+    case 'all':
+    default:
+      return [...tasks];
+  }
+}
